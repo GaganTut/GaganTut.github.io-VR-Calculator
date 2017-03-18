19077deb2f541cd752876d442233ab22d6edcedf
GaganTut <gstut@live.com>
2017-03-18 11:03:54 -1000

    Button Keys now have functioning visual events

--- js/app.js
+++ js/app.js
@@ -109,7 +109,7 @@ var frameModular = function() {
     position: "2 2 -6",
     material: "color: deepskyblue; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: /; color: white; align: center; font: dejavu; width: 10"
+    text : "value: Divided By; color: black; align: center; font: dejavu; width: 6"
   };
 
   var keyMltAtts = {
@@ -117,7 +117,7 @@ var frameModular = function() {
     position: "2 1 -6",
     material: "color: deepskyblue; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: *; color: white; align: center; font: dejavu; width: 10"
+    text : "value: Times; color: black; align: center; font: dejavu; width: 6"
   };
 
   var keySubAtts = {
@@ -125,7 +125,7 @@ var frameModular = function() {
     position: "2 0 -6",
     material: "color: deepskyblue; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: -; color: white; align: center; font: dejavu; width: 10"
+    text : "value: Minus; color: black; align: center; font: dejavu; width: 6"
   };
 
   var keyAddAtts = {
@@ -133,35 +133,35 @@ var frameModular = function() {
     position: "2 -1 -6",
     material: "color: deepskyblue; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: +; color: white; align: center; font: dejavu; width: 10"
+    text : "value: Plus; color: black; align: center; font: dejavu; width: 6"
   };
 
   var keyClrAtts = {
     position: "4 2 -6",
     material: "color: red; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: Clear; color: white; align: center; font: dejavu; width: 4"
+    text : "value: Clear; color: white; align: center; font: dejavu; width: 6"
   };
 
   var keyBalAtts = {
     position: "4 1 -6",
-    material: "color: royalblue; opacity: 0.75",
+    material: "color: limegreen; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: Get Balance; color: white; align: center; font: dejavu; width: 4"
+    text : "value: Get Balance; color: navy; align: center; font: dejavu; width: 6"
   };
 
   var keyDepAtts = {
     position: "4 0 -6",
-    material: "color: royalblue; opacity: 0.75",
+    material: "color: limegreen; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: Deposit Cash; color: white; align: center; font: dejavu; width: 4"
+    text : "value: Deposit Cash; color: navy; align: center; font: dejavu; width: 6"
   };
 
   var keyWthAtts = {
     position: "4 -1 -6",
-    material: "color: royalblue; opacity: 0.75",
+    material: "color: limegreen; opacity: 0.75",
     geometry : "primitive: plane; width: 2",
-    text : "value: Withdraw Cash; color: white; align: center; font: dejavu; width: 4"
+    text : "value: Withdraw Cash; color: navy; align: center; font: dejavu; width: 6"
   };
 
   var keyEquAtts = {
@@ -172,13 +172,50 @@ var frameModular = function() {
   };
 
   var animAtts = {
-    attribute: "material",
+    attribute: "rotation",
     begin: "myclick",
-    dur: "2000",
-    to: "color: white; opacity: 0",
-    repeat: 2,
+    dur: "300",
+    to: "360 0 0",
+    repeat: 1,
+    direction: "normal"
+  };
+
+  var operAtts = {
+    attribute: "rotation",
+    begin: "myclick",
+    dur: "300",
+    to: "0 0 360",
+    repeat: 1,
+    direction: "normal"
+  };
+
+  var explodeAnim = {
+    attribute: "scale",
+    begin: "clearClick",
+    dur: "500",
+    to: "0.1 0.1 0.1",
+    repeat: 1,
     direction: "alternate"
   };
+
+  var equalDisplayAtts = {
+    attribute: "position",
+    begin: "equalClick",
+    dur: "500",
+    to: "0 0 -0.5",
+    repeat: 1,
+    direction: "alternate"
+  };
+
+  var equalDisplayAtts2 = {
+    attribute: "rotation",
+    begin: "equalClick",
+    dur: "500",
+    to: "0 0 0",
+    repeat: 1,
+    direction: "alternate"
+  };
+
 
   function addAttribute(htmlID, attribute, value) { // Use ID Selector (ex. "#htmlID")
     var thisElement = document.querySelector(htmlID);
@@ -203,7 +240,7 @@ var frameModular = function() {
   function createEvent(parentID, attrObj) {
     var newAnim = document.createElement("a-animation");
     for (var objKey in attrObj) {
-      newAnim.objKey = attrObj[objKey];
+      newAnim.setAttribute(objKey, attrObj[objKey]);
     }
 
     var parent = document.querySelector(parentID);
@@ -233,92 +270,129 @@ var frameModular = function() {
   createframeElem("a-entity", "#frameScene", "btnWth", keyWthAtts);
   createframeElem("a-entity", "#frameScene", "btnEqu", keyEquAtts);
 
-  createEvent("#btn7", animAtts);
+
+  var topDisplay = document.querySelector("#frameDisplay");
 
   var key7 = document.querySelector("#btn7");
   key7.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
     key7.emit("myclick");
+
+    console.log(event);
   });
 
   var key8 = document.querySelector("#btn8");
   key8.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key8.emit("myclick");
   });
 
   var key9 = document.querySelector("#btn9");
   key9.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key9.emit("myclick");
   });
 
   var key6 = document.querySelector("#btn6");
   key6.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key6.emit("myclick");
   });
 
   var key5 = document.querySelector("#btn5");
   key5.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key5.emit("myclick");
   });
 
   var key4 = document.querySelector("#btn4");
   key4.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key4.emit("myclick");
   });
 
   var key3 = document.querySelector("#btn3");
   key3.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key3.emit("myclick");
   });
 
   var key2 = document.querySelector("#btn2");
   key2.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key2.emit("myclick");
   });
 
   var key1 = document.querySelector("#btn1");
   key1.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key1.emit("myclick");
   });
 
   var key0 = document.querySelector("#btn0");
   key0.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key0.emit("myclick");
   });
 
   var key00 = document.querySelector("#btn00");
   key00.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    key00.emit("myclick");
   });
 
   var keyDot = document.querySelector("#btnDot");
   keyDot.addEventListener("click", function(event) {
     cashMod.clickNewNum(event.target.attributes.keyVal.value);
+    keyDot.emit("myclick");
   });
 
   var keyDiv = document.querySelector("#btnDiv");
   keyDiv.addEventListener("click", function(event) {
     cashMod.clickOperation(event.target.attributes.keyVal.value);
+    keyDiv.emit("myclick");
   });
 
   var keyMlt = document.querySelector("#btnMlt");
   keyMlt.addEventListener("click", function(event) {
     cashMod.clickOperation(event.target.attributes.keyVal.value);
+    keyMlt.emit("myclick");
   });
 
   var keySub = document.querySelector("#btnSub");
   keySub.addEventListener("click", function(event) {
     cashMod.clickOperation(event.target.attributes.keyVal.value);
+    keySub.emit("myclick");
   });
 
   var keyAdd = document.querySelector("#btnAdd");
   keyAdd.addEventListener("click", function(event) {
     cashMod.clickOperation(event.target.attributes.keyVal.value);
+    keyAdd.emit("myclick");
   });
 
   var keyClr = document.querySelector("#btnClr");
   keyClr.addEventListener("click", function(event) {
     cashMod.clickClear();
+    keyClr.emit("clearClick");
+    key9.emit("clearClick");
+    key8.emit("clearClick");
+    key7.emit("clearClick");
+    key6.emit("clearClick");
+    key5.emit("clearClick");
+    key4.emit("clearClick");
+    key3.emit("clearClick");
+    key2.emit("clearClick");
+    key1.emit("clearClick");
+    key0.emit("clearClick");
+    key00.emit("clearClick");
+    keyDot.emit("clearClick");
+    keyDiv.emit("clearClick");
+    keyMlt.emit("clearClick");
+    keySub.emit("clearClick");
+    keyAdd.emit("clearClick");
+
+
   });
 
   var keyBal = document.querySelector("#btnBal");
@@ -339,6 +413,49 @@ var frameModular = function() {
   var keyEqu = document.querySelector("#btnEqu");
   keyEqu.addEventListener("click", function(event) {
     cashMod.equalSign();
+    topDisplay.emit("equalClick");
   });
+
+  createEvent("#btn9", animAtts);
+  createEvent("#btn8", animAtts);
+  createEvent("#btn7", animAtts);
+  createEvent("#btn6", animAtts);
+  createEvent("#btn5", animAtts);
+  createEvent("#btn4", animAtts);
+  createEvent("#btn3", animAtts);
+  createEvent("#btn2", animAtts);
+  createEvent("#btn1", animAtts);
+  createEvent("#btn0", animAtts);
+  createEvent("#btn00", animAtts);
+  createEvent("#btnDot", animAtts);
+
+  createEvent("#btnDiv", operAtts);
+  createEvent("#btnMlt", operAtts);
+  createEvent("#btnSub", operAtts);
+  createEvent("#btnAdd", operAtts);
+
+
+
+  createEvent("#btnClr", explodeAnim);
+  createEvent("#btn9", explodeAnim);
+  createEvent("#btn8", explodeAnim);
+  createEvent("#btn7", explodeAnim);
+  createEvent("#btn6", explodeAnim);
+  createEvent("#btn5", explodeAnim);
+  createEvent("#btn4", explodeAnim);
+  createEvent("#btn3", explodeAnim);
+  createEvent("#btn2", explodeAnim);
+  createEvent("#btn1", explodeAnim);
+  createEvent("#btn0", explodeAnim);
+  createEvent("#btn00", explodeAnim);
+  createEvent("#btnDot", explodeAnim);
+  createEvent("#btnDiv", explodeAnim);
+  createEvent("#btnMlt", explodeAnim);
+  createEvent("#btnSub", explodeAnim);
+  createEvent("#btnAdd", explodeAnim);
+
+
+  createEvent("#frameDisplay", equalDisplayAtts);
+  createEvent("#frameDisplay", equalDisplayAtts2);
 
 };
